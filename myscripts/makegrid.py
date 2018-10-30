e5ab428132af69b5ae0b0d98a0f0afcb86713c29
Jincheng Zhang <zhjch05@live.com>
2018-10-30 00:12:01 -0400

    new script for transit

--- myscripts/makegrid.py
+++ myscripts/makegrid.py
@@ -39,8 +39,16 @@ with open("output/infrastructure", "w") as infra_output:
                 infra_output.write(chr(65 + i) + str(j + 1) +
                                    "," + chr(66 + i) + str(j + 1) + "\n")
 with open("output/transit", "w") as transit_output:
+    nodes = []
     for i in range(0, row):
         for j in range(0, col):
-            for  _i in range(i+1, row):
-                for  _j in range(j+1, col):
-                    transit_output.write(chr(65+i)+str(j+1)+","+chr(65+_i)+str(_j+1)+",500\n")
+            nodes.append(chr(65+i)+str(j+1))
+
+    for i in range(0, len(nodes)):
+        for j in range(i, len(nodes)):
+            transit_output.write(nodes[i]+","+nodes[j]+",500\n")
+    # for i in range(0, row):
+    #     for j in range(0, col):
+    #         for  _i in range(i+1, row):
+    #             for  _j in range(j+1, col):
+    #                 transit_output.write(chr(65+i)+str(j+1)+","+chr(65+_i)+str(_j+1)+",500\n")
